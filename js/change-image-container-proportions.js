8d3aa839dbc2a7a5688183fda8448df90583f37b
Людмила <kornilova203@gmail.com>
2016-12-19 17:44:19 +0300

    remove bottom panel on iphone

--- js/change-image-container-proportions.js
+++ js/change-image-container-proportions.js
@@ -29,9 +29,7 @@ function changeProps (i) {
 }
 
 changeAllProps();
-if(isMobile.any())  //убираем нижнюю панель на мобильных
-  window.addEventListener("load", function() { window. scrollTo(0, 0); });
-else //если мы заходим с компа, то делать ресайз
+if(!isMobile.any()) //если мы заходим с компа, то делать ресайз
   window.addEventListener("resize", changeAllProps);
 
 function changeAllProps() {
