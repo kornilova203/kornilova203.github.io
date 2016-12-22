cf91bcc21f14ede8a9867b2abf5f09a0f9564bc2
Людмила <kornilova203@gmail.com>
2016-12-22 16:37:12 +0300

    Update index.js

--- telegram/index.js
+++ telegram/index.js
@@ -6,7 +6,7 @@ xhr.open('GET', 'https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl
 xhr.open('GET', 'https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI/sendmessage?chat_id=52331883&text=hilusi', false);
 
 var jsonToJs = JSON.parse(xhr.responseText);
-alert('hello');
+alert('hello!');
 alert(jsonToJs.ok);
 
 // 3. Отсылаем запрос
