46d6264cf195177c4cc605c4ed5415a3364d5cb3
Людмила <kornilova203@gmail.com>
2016-12-22 14:27:05 +0300

    add <?php

--- telegram/index.php
+++ telegram/index.php
@@ -1,4 +1,8 @@
+<?php
+
 $botToken = "321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI";
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents($website."/getupdates");
 print_r($update);
+
+?>
