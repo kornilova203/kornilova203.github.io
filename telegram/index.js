595e18610653c14b92c068f381cef946b26c1990
Людмила <kornilova203@gmail.com>
2016-12-22 15:59:03 +0300

    Update index.js

--- telegram/index.js
+++ telegram/index.js
@@ -3,7 +3,7 @@ var xhr = new XMLHttpRequest();
 
 // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
 xhr.open('GET', 'https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI/getMe', false);
-xhr.open('GET', 'https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI/sendmessage?chat_id=52331883&text=hi', false);
+xhr.open('GET', 'https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI/sendmessage?chat_id=52331883&text=hi, lusi', false);
 
 
 // 3. Отсылаем запрос
