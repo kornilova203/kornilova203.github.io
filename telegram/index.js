0053d82f95ff08cd82088f7d3bcf474e1289d7d5
Людмила <kornilova203@gmail.com>
2016-12-22 15:36:37 +0300

    Update index.js

--- telegram/index.js
+++ telegram/index.js
@@ -1,1 +1,33 @@
-alert(https://api.telegram.org/bot321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI/getMe);
+var TelegramBot = require('node-telegram-bot-api');
+ 
+var token = '321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
+});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
