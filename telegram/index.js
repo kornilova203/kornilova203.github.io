cff6d669f6a56d277a4d871ee49eb780418c6ff6
Людмила <kornilova203@gmail.com>
2016-12-22 15:46:30 +0300

    Update index.js

--- telegram/index.js
+++ telegram/index.js
@@ -1,33 +1,17 @@
-var TelegramBot = require('node-telegram-bot-api');
- 
-var token = '321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI';
-var botOptions = {
-    polling: true
-};
-var bot = new TelegramBot(token, botOptions);
- 
-bot.getMe().then(function(me)
-{
-    console.log('Hello! My name is %s!', me.first_name);
-    console.log('My id is %s.', me.id);
-    console.log('And my username is @%s.', me.username);
-});
- 
-bot.on('text', function(msg)
-{
-    var messageChatId = msg.chat.id;
-    var messageText = msg.text;
-    var messageDate = msg.date;
-    var messageUsr = msg.from.username;
- 
-    if (messageText === '/say') {
-        sendMessageByBot(messageChatId, 'Hello World!');
-    }
- 
-    console.log(msg);
-});
- 
-function sendMessageByBot(aChatId, aMessage)
-{
-    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+// 1. Создаём новый объект XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+xhr.open('GET', 'index.html', false);
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Если код ответа сервера не 200, то это ошибка
+if (xhr.status != 200) {
+  // обработать ошибку
+  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+  // вывести результат
+  alert( xhr.responseText ); // responseText -- текст ответа.
 }
