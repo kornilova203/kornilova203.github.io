1a689834071d2d6928cde10599dc38c6e5e2b4af
Lyudmila Kornilova <kornilova203@gmail.com>
2017-01-25 16:13:42 +0300

    images on canvas

---
+++ js/ball.js
@@ -0,0 +1,38 @@
+// dependencies: Vector2D
+function Ball(img, radius, mass){
+	if(typeof(radius)==='undefined') radius = 20;
+	if(typeof(mass)==='undefined') mass = 1;
+	this.radius = radius;
+	this.mass = mass;
+	this.x = 0;
+	this.y = 0;
+	this.vx = 0;
+	this.vy = 0;
+	this.img = img;
+	// image = new Image();
+
+	// image.onload = function() {
+		// context.drawImage(image, 69, 50);
+	// };
+	// image.src = 'img/space/sun-on.png';
+}
+
+Ball.prototype = {
+	get pos2D (){
+		return new Vector2D(this.x,this.y);
+	},
+	set pos2D (pos){
+		this.x = pos.x;
+		this.y = pos.y;
+	},
+	get velo2D (){
+		return new Vector2D(this.vx,this.vy);
+	},
+	set velo2D (velo){
+		this.vx = velo.x;
+		this.vy = velo.y;
+	},
+	draw: function (context) {
+		context.drawImage(this.img, this.x-this.radius, this.y-this.radius);
+	}
+};
--- js/orbits.js
+++ js/orbits.js
@@ -1,4 +1,3 @@
-var imgs = document.getElementsByClassName('planet'); //array of planets
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var canvas_bg = document.getElementById('canvas_bg');
@@ -10,33 +9,23 @@ var m = 1; // planet's mass
 var M = 1000000; // sun's mass
 var G = 1;
 var t0,dt;
-var vh;
-var vw;
-var body;
 
 window.onload = init;
 
 function init() {
-  context.canvas.width  = window.innerWidth;
-  context.canvas.height = window.innerHeight;
-  context_bg.canvas.width  = window.innerWidth;
-  context_bg.canvas.height = window.innerHeight;
-  body = document.getElementsByTagName("body")[0];
-  vh = body.clientHeight;
-  vw = body.clientWidth;
 	// create 100 stars randomly positioned
-	// for (var i=0; i<100; i++){
-	// 	var star = new Ball(Math.random()*2,'#ffff00');
-	// 	star.pos2D = new Vector2D(Math.random()*canvas_bg.width,Math.random()*canvas_bg.height);
-	// 	star.draw(context_bg);
-	// }
+	for (var i=0; i<100; i++){
+		var star = new Ball(Math.random()*2,'#ffff00');
+		star.pos2D = new Vector2D(Math.random()*canvas_bg.width,Math.random()*canvas_bg.height);
+		star.draw(context_bg);
+	}
 	// create a stationary sun
-	sun = new Ball(imgs[0], 170, M);
-	sun.pos2D = new Vector2D(vw/2,vh/2);
+	sun = new Ball(70,'#ff9900',M,0,true);
+	sun.pos2D = new Vector2D(400,300);
 	sun.draw(context_bg);
 	// create a moving planet
-	planet = new Ball(imgs[1], 100, m);
-	planet.pos2D = new Vector2D(vw/2,vh/2-250);
+	planet = new Ball(10,'#0000ff',m,0,true);
+	planet.pos2D = new Vector2D(400,50);
   // var r = planet.pos2D.subtract(sun.pos2D).lenght();
   var v = Math.sqrt(G*M*m/250); //velocity for circular orbit
 	planet.velo2D = new Vector2D(v, 0);
